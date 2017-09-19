487ab788f00f2450291c3e699bcac791c7584262
Trung Ke <trungke21@gmail.com>
2017-09-19 09:07:16 +0700

    1
    up

---
+++ src/Http/Controllers/FaceswapController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Esdetect\Faceswap\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+use Carbon\Carbon;
+
+class FaceswapController extends BaseController
+{
+    static public function index()
+    {
+        return 'trung';
+    }
+}
